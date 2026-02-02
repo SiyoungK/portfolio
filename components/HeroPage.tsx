569201334e1d47e9c845b783efda2cfa497219a9
SiyoungK <siyoungkim307@gmail.com>
2026-02-02 15:18:57 -0800

    Add subtitle styling (h3) and resize hero page image

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function HeroPage() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 h-[85vh] max-w-7xl mx-auto">
+    <section className="grid grid-cols-1 md:grid-cols-2 h-[85vh] max-w-[100rem] mx-auto">
       {/* Text Section */}
       <div className="basis-1/2 my-auto">
         <div className="text-left p-10">
-          <h1 className="text-4xl pb-4">Siyoung Kim</h1>
-          <p className="pb-6">
-            Frontend Web Developer & UI/UX Designer.
-          </p>
+          <h1 className="text-7xl pb-4">Siyoung Kim</h1>
+          <h3 className="pb-6">
+            Frontend Web Developer & UI/UX Designer
+          </h3>
           <div className="flex gap-4">
             <button className="bg-black text-white py-2 px-5 rounded-xl">Contact</button>
             <button className="bg-black text-white py-2 px-5 rounded-xl">Resume</button>
@@ -18,8 +18,8 @@ export default function HeroPage() {
       </div>
       <div className="basis-1/2 my-auto">
         {/* Image Section */}
-        <div className="static rounded-xl h-[450px] w-[600px] align-left drop-shadow-xl/50">
-          <div className="absolute h-[418px] w-[600px]">
+        <div className="static rounded-xl h-[527px] w-[826px] align-left drop-shadow-xl/50">
+          <div className="absolute h-full w-full">
             <Image 
               alt="Alter Alley, a website for artists to share alters of Magic the Gathering cards." 
               src="/alteralley-home.png"
@@ -29,7 +29,7 @@ export default function HeroPage() {
           </div>
           {/* Mac window bar */}
           {/* TODO: Replace with svg version to scale easily, and be more optimal than divs */}
-          <div className="bg-white absolute h-[32px] w-[600px] rounded-t-xl align-top drop-shadow-sm">
+          <div className="bg-white absolute h-[32px] w-full rounded-t-xl align-top drop-shadow-sm">
             <div className="flex gap-[6px] p-[12px]">
               <div className="bg-red-400 size-[9px] rounded-full"/>
               <div className="bg-yellow-400 size-[9px] rounded-full"/>
