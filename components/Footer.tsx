28dccd9e8467f573b2822927436f851b85b92d92
SiyoungK <siyoungkim307@gmail.com>
2026-02-13 11:08:59 -0800

    Add md display for Footer, icon svg syntax fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,25 @@ export default function Footer() {
     return (
         <footer className="flex justify-left py-10 border-t border-gray-300 text-footer-text">
           <div className="mx-auto w-[90%] md:max-w-[90%]">
-            {/* === Useful links === */}
-            <div className="py-5 flex justify-start flex-col gap-3">
-              <a>
-                Resume
-              </a>
-              <a>
-                Contact
-              </a>
-            </div>
-            {/* === Socials === */}
-            <div className="py-5 flex flex-row gap-4">
-              <a>
-                <LinkedInIcon className="w-5 aspect-square" fillColor="fill-footer-text"/>
-              </a>
-              <a>
-                <GitHubIcon className="w-5 aspect-square" fillColor="fill-footer-text"/>
-              </a>
+            <div className="md:flex md:flex-row md:justify-between">
+              {/* === Useful links === */}
+              <div className="py-5 flex justify-start flex-col md:flex-row gap-3">
+                <a>
+                  Resume
+                </a>
+                <a>
+                  Contact
+                </a>
+              </div>
+              {/* === Socials === */}
+              <div className="py-5 flex flex-row gap-4">
+                <a>
+                  <LinkedInIcon className="w-5 aspect-square" fillColor="fill-footer-text"/>
+                </a>
+                <a>
+                  <GitHubIcon className="w-5 aspect-square" fillColor="fill-footer-text"/>
+                </a>
+              </div>
             </div>
             {/* Copyright */}
             <div className="py-5">
