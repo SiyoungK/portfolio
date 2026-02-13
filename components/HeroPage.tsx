b971ba9aed1767bdcc7558affc5d9dfdf3b19a58
SiyoungK <siyoungkim307@gmail.com>
2026-02-13 15:23:07 -0800

    Add gradient and temp mac window border to HeroPage

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -4,8 +4,12 @@ import BasicButton from "./BasicButton";
 export default function HeroPage() {
   return (
     <section className="overflow-x-hidden">
-      {/* Text */}
-      <div className="max-w-400 md:grid md:grid-cols-2 md:h-180 md:max-h-[70vh] mx-auto ">
+      <div className="relative max-w-400 md:grid md:grid-cols-2 md:h-180 md:max-h-[70vh] mx-auto">
+        {/* Gradient */}
+        <div className="absolute -z-10 bg-radial from-indigo-400 to-transparent to-70% blur-2xl
+        w-[90%] h-[35%] bottom-0 left-0 right-0 m-auto
+        md:w-[40%] md:h-[75%] md:top-0 md:left-3/7 md:right-auto"/>
+        {/* Text */}
         <div className="md:basis-1/2 md:my-auto">
           <div className="text-center md:text-left py-10 md:p-10 wrap-break-word">
             <h1 className="text-7xl pb-4">Siyoung Kim</h1>
@@ -19,9 +23,11 @@ export default function HeroPage() {
           </div>
         </div>
         {/* Image */}
-        <div className="px-2 md:h-full flex justify-center md:justify-start md:basis-1/2 md:my-auto md:items-center md:overflow-x-hidden lg:overflow-visible">
-          <div className="relative w-[80%] md:w-auto md:h-[90%] aspect-video rounded-xl shadow-md/50 overflow-clip">
-              <Image 
+        <div className="md:pl-10 md:h-full flex justify-center md:justify-start md:basis-1/2 md:my-auto md:items-center md:overflow-x-hidden lg:overflow-visible">
+          <div className="relative w-[80%] md:w-auto md:h-[90%] aspect-video overflow-clip shadow-xl
+            border border-t-10 border-gray-300 rounded-sm
+            md:border-3 md:border-t-35 md:rounded-xl">
+            <Image 
               alt="Alter Alley, a website for artists to share alters of Magic the Gathering cards." 
               src="/AlterAlleyHome.png"
               className="object-cover object-top-left"
