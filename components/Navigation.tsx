155d5c47ef78bb8acdb38a20ffd03c2d48b5014a
SiyoungK <siyoungkim307@gmail.com>
2026-02-17 00:06:37 -0800

    Swap navbar to white bg

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,24 +3,24 @@ import BasicButton from "./BasicButton";
 export default function Navigation() {
   return (
     <nav className="flex justify-center">
-      <div className="bg-black flex fixed justify-between w-[80%] md:w-[50%] my-5 mx-auto p-2 rounded-xl z-9999">
+      <div className="bg-white border-2 border-black flex fixed justify-between w-[80%] md:w-[50%] my-5 mx-auto p-2 rounded-xl z-9999">
         {/* Logo */}
-        <button className="text-white py-2 px-5 rounded-xl">
+        <button className="text-black py-2 px-5 rounded-xl">
             SK
         </button>
         {/* Links */}
         <div className="flex space-x-4 items-center">
           <div className="hidden md:block">
-            <button className="text-white py-2 px-5 rounded-xl">
+            <button className="text-black py-2 px-5 rounded-xl">
               Portfolio
             </button>
-            <button className="text-white py-2 px-5 rounded-xl">
+            <button className="text-black py-2 px-5 rounded-xl">
               Experience
             </button>
           </div>
           <div>
             <BasicButton text="Contact" external={true} href="https://www.linkedin.com/in/siyoung/" 
-              className="text-black bg-white hover:bg-zinc-300 border-white hover:border-zinc-300"/>
+              className="text-black bg-white hover:bg-zinc-300 border-black hover:border-zinc-300"/>
           </div>
         </div>
       </div>
