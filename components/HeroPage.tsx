35c1dc0c01f9e76bb75e79f086795b21ba272c9d
SiyoungK <siyoungkim307@gmail.com>
2026-02-18 11:44:46 -0800

    Fix sm rolodex sizing

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -30,7 +30,7 @@ export default function HeroPage() {
       </div>
       {/* Rolodex */}
       <div className="md:basis-1/2 flex items-center justify-center">
-        <motion.div className="aspect-square w-180"
+        <motion.div className="aspect-square w-[80%] md:w-180"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
